5a04dbe1d6d3bb45a33b93c4f779d6f0bcd985af
Saurabh <saurabh.banore@townscript.com>
2019-09-22 22:05:32 +0530

    implemented htmlStatement

--- src/main/java/com/movierental/Customer.java
+++ src/main/java/com/movierental/Customer.java
@@ -20,48 +20,37 @@ public class Customer {
   }
 
   public String statement() {
-    double totalAmount = 0;
-    int frequentRenterPoints = 0;
-    String result = "Rental Record for " + getName() + "\n";
-    for (Rental each : rentals) {
-      double thisAmount1 = each.amountFor();
-	double thisAmount = thisAmount1;
-      frequentRenterPoints = each.frequentRentalPoints(frequentRenterPoints);
+	    String result = "Rental Record for " + getName() + "\n";
+		double totalAmount = totalAmount();
+	    int frequentRenterPoints = freeRentalPoints();
+  	for (Rental each : rentals) {
+  		//show figures for this rental
+  		result += "\t" + each.getMovie().getTitle() + "\t" +
+		     String.valueOf(each.amountFor()) + "\n";
 
-      //show figures for this rental
-      result += "\t" + each.getMovie().getTitle() + "\t" +
-          String.valueOf(thisAmount) + "\n";
-      totalAmount += thisAmount;
-    }
-
-    //add footer lines result
-    result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-    result += "You earned " + String.valueOf(frequentRenterPoints)
-        + " frequent renter points";
-    return result;
+	    }
+	    //add footer lines result
+	    result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+	    result += "You earned" + String.valueOf(frequentRenterPoints)
+	        + " frequent renter points";
+	    return result;	
   }
 
 //Provides the Statement as an HTML String
   public String htmlStatement() {
-	    
-	    
-	    
 	    String result = "<h1>Rental Record for " + getName() + "<h1><br/>";
 		double totalAmount = totalAmount();
 	    int frequentRenterPoints = freeRentalPoints();
     	for (Rental each : rentals) {
-	
     		//show figures for this rental
     		result += "\t" + each.getMovie().getTitle() + "\t" +
-		     String.valueOf(each.amountFor()) + "\n";
+		     String.valueOf(each.amountFor()) + "<br/>";
 
 	    }
-
 	    //add footer lines result
 	    result += "Amount owed is <b>" + String.valueOf(totalAmount) + "</b><br/>";
 	    result += "You earned <b>" + String.valueOf(frequentRenterPoints)
 	        + "</b> frequent renter points";
-	    
 	    return result;	
   }
 
--- src/test/java/com/movierental/CustomerTest.java
+++ src/test/java/com/movierental/CustomerTest.java
@@ -2,25 +2,45 @@ package com.movierental;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CustomerTest {
-    @Test
-    public void TestGeneratedStatement(){
+    
+	private Customer customer;
+	@Before
+    public void initialData(){
     	
-    	Customer customer = new Customer("Saurabh");
+    	customer = new Customer("Saurabh");
     	customer.addRental(new Rental(new Movie("John Wick", Movie.REGULAR), 5));
     	customer.addRental(new Rental(new Movie("Up", Movie.CHILDRENS), 15));
     	customer.addRental(new Rental(new Movie("Batla House", Movie.NEW_RELEASE), 1));
     	
+    }
+    
+    @Test
+    public void generateTextStatement() {
     	assertEquals("Rental Record for Saurabh\n" + 
     			"	John Wick	6.5\n" + 
     			"	Up	19.5\n" + 
     			"	Batla House	3.0\n" + 
     			"Amount owed is 29.0\n" + 
     			"You earned 3 frequent renter points", customer.statement());
-    	
-    	
+    
     }
+    	
+    @Test
+    public void generateHTMLStatement() {
+    	assertEquals("Rental Record for Saurabh\n" + 
+    			"	John Wick	6.5\n" + 
+    			"	Up	19.5\n" + 
+    			"	Batla House	3.0\n" + 
+    			"Amount owed is 29.0\n" + 
+    			"You earned 3 frequent renter points", customer.htmlStatement());
+    
+    }
+    	
+    	
+    
 
 }

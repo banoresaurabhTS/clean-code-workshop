aeca30b20f2cb2264c1bc044a1dbd14f0bce9e4d
Saurabh <saurabh.banore@townscript.com>
2019-09-22 20:14:08 +0530

    Added the HTMLstatement Method and Got rid of code envy

--- src/main/java/com/movierental/Customer.java
+++ src/main/java/com/movierental/Customer.java
@@ -24,23 +24,24 @@ public class Customer {
     int frequentRenterPoints = 0;
     String result = "Rental Record for " + getName() + "\n";
     for (Rental each : rentals) {
-      double thisAmount = 0;
-      //determine amounts for each line
-      switch (each.getMovie().getPriceCode()) {
-        case Movie.REGULAR:
-          thisAmount += 2;
-          if (each.getDaysRented() > 2)
-            thisAmount += (each.getDaysRented() - 2) * 1.5;
-          break;
-        case Movie.NEW_RELEASE:
-          thisAmount += each.getDaysRented() * 3;
-          break;
-        case Movie.CHILDRENS:
-          thisAmount += 1.5;
-          if (each.getDaysRented() > 3)
-            thisAmount += (each.getDaysRented() - 3) * 1.5;
-          break;
-      }
+      double thisAmount1 = 0;
+		  //determine amounts for each line
+		  switch (each.getMovie().getPriceCode()) {
+		    case Movie.REGULAR:
+		      thisAmount1 += 2;
+		      if (each.getDaysRented() > 2)
+		        thisAmount1 += (each.getDaysRented() - 2) * 1.5;
+		      break;
+		    case Movie.NEW_RELEASE:
+		      thisAmount1 += each.getDaysRented() * 3;
+		      break;
+		    case Movie.CHILDRENS:
+		      thisAmount1 += 1.5;
+		      if (each.getDaysRented() > 3)
+		        thisAmount1 += (each.getDaysRented() - 3) * 1.5;
+		      break;
+		  }
+	double thisAmount = thisAmount1;
       // add frequent renter points
       frequentRenterPoints++;
       // add bonus for a two day new release rental
@@ -59,6 +60,58 @@ public class Customer {
     result += "You earned " + String.valueOf(frequentRenterPoints)
         + " frequent renter points";
     return result;
+  }
+
+ //Provides the Statement as an HTML String
+  public String htmlStatement() {
+	    
+	  double totalAmount = 0;
+	    
+	    int frequentRenterPoints = 0;
+	    
+	    String result = "<h1>Rental Record for " + getName() + "<h1><br/>";
+	    
+	    for (Rental each : rentals) {
+	      
+	      double thisAmount1 = 0;
+			  //determine amounts for each line
+			  switch (each.getMovie().getPriceCode()) {
+			    case Movie.REGULAR:
+			      thisAmount1 += 2;
+			      if (each.getDaysRented() > 2)
+			        thisAmount1 += (each.getDaysRented() - 2) * 1.5;
+			      break;
+			    case Movie.NEW_RELEASE:
+			      thisAmount1 += each.getDaysRented() * 3;
+			      break;
+			    case Movie.CHILDRENS:
+			      thisAmount1 += 1.5;
+			      if (each.getDaysRented() > 3)
+			        thisAmount1 += (each.getDaysRented() - 3) * 1.5;
+			      break;
+			  }
+		double thisAmount = thisAmount1;
+	     
+	      // add frequent renter points
+	      frequentRenterPoints++;
+	      
+	      // add bonus for a two day new release rental
+	      if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
+	          &&
+	          each.getDaysRented() > 1) frequentRenterPoints++;
+
+	      //show figures for this rental
+	      result += "&nbsp &nbsp &nbsp &nbsp &nbsp" + each.getMovie().getTitle() + "&nbsp &nbsp &nbsp &nbsp &nbsp" +
+	          String.valueOf(thisAmount) + "<br/>";
+	      totalAmount += thisAmount;
+	    }
+
+	    //add footer lines result
+	    result += "Amount owed is <b>" + String.valueOf(totalAmount) + "</b><br/>";
+	    result += "You earned <b>" + String.valueOf(frequentRenterPoints)
+	        + "</b> frequent renter points";
+	    
+	    return result;	
   }
 }
 
--- src/test/java/com/movierental/CustomerTest.java
+++ src/test/java/com/movierental/CustomerTest.java
@@ -1,9 +1,26 @@
 package com.movierental;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class CustomerTest {
     @Test
-    public void test(){}
+    public void TestGeneratedStatement(){
+    	
+    	Customer customer = new Customer("Saurabh");
+    	customer.addRental(new Rental(new Movie("John Wick", Movie.REGULAR), 5));
+    	customer.addRental(new Rental(new Movie("Up", Movie.CHILDRENS), 15));
+    	customer.addRental(new Rental(new Movie("Batla House", Movie.NEW_RELEASE), 1));
+    	
+    	assertEquals("Rental Record for Saurabh\n" + 
+    			"	John Wick	6.5\n" + 
+    			"	Up	19.5\n" + 
+    			"	Batla House	3.0\n" + 
+    			"Amount owed is 29.0\n" + 
+    			"You earned 3 frequent renter points", customer.statement());
+    	
+    	
+    }
 
 }
